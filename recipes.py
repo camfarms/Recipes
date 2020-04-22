cf506bc6dd0eb07b7990a79a48a0c5d3e049764d
Samantha Frankum <srf48@case.edu>
2020-04-21 18:59:56 -0700

    Added food joke and search recipes

--- recipes.py
+++ recipes.py
@@ -2,7 +2,20 @@
 """
 Created on Tue Apr 21 18:55:03 2020
 
-@author: samor
+@author: THE BEST TEAMMMMM evaaaa.... not
 """
+import spoonacular as sp
+api = sp.API("your_api_key_here")
 
 
+def searchrecipesbyingredients(ingredients, fillIngredients=None, 
+                              limitLicense=None, number=None, ranking=None ):
+    searchResults = api.search_recipes_by_ingredients(ingredients, fillIngredients=None, 
+                limitLicense=None, number=None, ranking=None )
+    data = searchResults.json()
+    return data
+    
+
+def getfoodjoke():
+    foodJoke = api.get_a_random_food_joke()
+    return foodJoke.json()
